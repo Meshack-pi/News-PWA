b79d3bb7659e6074159bc0930c9d75ce1ba26ea4
Meshack-pi <kipronomeshack36@gmail.com>
2022-01-13 18:31:35 +0300

    Added files to the directory

---
+++ newsApi.js
@@ -0,0 +1,5 @@
+const apiKey = '774e253815c24dbc816d4db1feab29d2';
+const topHeadlinesUrl =
+  'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + apiKey;
+
+export { apiKey, topHeadlinesUrl };
